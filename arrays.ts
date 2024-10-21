f8335ec878c592681f0e84b3026455f5069e3897
daniel-tsonkov <daniel_tzonkov@abv.bg>
2024-10-21 16:00:51 +0300

    up

--- arrays.ts
+++ arrays.ts
@@ -1,10 +1,18 @@
-const carMakers = ['ford', 'royota', 'chevy'];
-const dates = [new Date(), new Date()];
+// const carMakers = ['ford', 'royota', 'chevy'];
+// const dates = [new Date(), new Date()];
 
-const carsByMake = [['f15'], ['corolla'], ['camaro']];
+// const carsByMake = [['f15'], ['corolla'], ['camaro']];
 
-const data = ['asd', 123];
+// const data = ['asd', 123];
 
-carMakers.push(data[0].toString());
+// carMakers.push(data[0].toString());
 
-console.log(carMakers[3]);
+// console.log(carMakers[3]);
+
+const drink = {
+  color: 'blue',
+  carbonated: true,
+  sugar: 40,
+};
+
+const pepsi = ['blue', true, 40];
